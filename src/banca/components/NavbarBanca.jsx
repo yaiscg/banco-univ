fe30ad34ca6f14805a602c9da610350bf2043f7e
yaiscg <yaisraylith@gmail.com>
2023-08-22 13:48:51 -0400

    Casi lista pagina principal de la banca

--- src/banca/components/NavbarBanca.jsx
+++ src/banca/components/NavbarBanca.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { BsChevronDown } from "react-icons/bs";
+import { CiLogout } from "react-icons/ci";
+import { TbArrowsExchange2 } from "react-icons/tb";
+import { LuContact } from "react-icons/lu";
+import { AiOutlineFileSearch } from "react-icons/ai";
+
+
+
 
 const NavbarBanca = () => {
 
@@ -11,19 +18,39 @@ const NavbarBanca = () => {
                 <div className="px-10 flex justify-between w-full h-full">
                     
                     <div className="flex items-center">
-                    <ul className='font-Montserrat hidden md:flex '>  
+                        <div className='font-Montserrat hidden md:flex '>  
                         
-                        <div className="hidden md:flex">
-                            <li>Consultas</li>
-                        </div>
-                        
-                        <div>
-                            <li>Transferencias</li>
-                        </div>
+                        <button className="bg-transparent border-transparent text-black px-6">
+                            <div className="hidden md:flex items-center">
+                            <AiOutlineFileSearch className="px-1 w-7"/>
+                            <h1>Consultas</h1>
+                            <BsChevronDown className="px-1 w-7"/>
+                            </div>
+                        </button>
 
-                        <li>Agenda</li>
-                        <li>Salir</li>
-                    </ul>
+                        <button className="bg-transparent border-transparent text-black px-6">
+                            <div className="hidden md:flex items-center">
+                            <TbArrowsExchange2 className="px-1 w-7"/>
+                            <h1>Transferencias</h1>
+                            <BsChevronDown className="px-1 w-7"/>
+                            </div>
+                        </button>
+
+                        <button className="bg-transparent border-transparent text-black px-6">
+                            <div className="hidden md:flex items-center">
+                            <LuContact className="px-1 w-7"/>
+                            <h1>Agenda</h1>
+                            <BsChevronDown className="px-1 w-7"/>
+                            </div>
+                        </button>
+
+                        <button className="bg-transparent border-transparent text-black px-12">
+                            <div className="hidden md:flex items-center">
+                            <h1>Salir</h1>
+                            <CiLogout className="px-1 w-7"/>
+                            </div>
+                        </button>
+                        </div>
                     </div>
 
                 </div>
