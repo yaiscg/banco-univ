03ede0bdb01bb29361176b107e3e6173ff6a8605
Mariel García <magbabio@gmail.com>
2023-09-01 19:37:09 -0400

    Saldo actualizado

--- src/banca/components/Saldo.jsx
+++ src/banca/components/Saldo.jsx
@@ -16,7 +16,7 @@ const Saldo = ({visible, onClose}) => {
     className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm
     flex justify-center items-center"
     >
-      <div className="bg-white px-10 py-12 rounded">
+      <div className="bg-white px-10 py-12 rounded-2xl">
         <span className="flex items-center justify-center mb-3 text-xl font-bold font-Montserrat text-primary">Saldo disponible</span>
         <h1 className="px-2 py-2 text-center">3.500 BS</h1>
         <button onClick={onClose} className="w-full text-white py-3 px-6 rounded-lg mb-6 bg-secondary border-secondary">Regresar</button>
