23183196036d8f621d3e7308dcd681bcc224c966
Mariel García <magbabio@gmail.com>
2023-09-02 23:58:22 -0400

    Validaciones gestión contraseña

--- src/pages/Contrasenna.jsx
+++ src/pages/Contrasenna.jsx
@@ -1,49 +1,25 @@
 import React, {useState} from 'react';
-import { Link } from "react-router-dom";
-import {RiUser3Line, RiLockLine, RiEyeLine, RiEyeOffLine} from "react-icons/ri"
-import { toast } from "react-toastify";
+import {RiMailLine, RiLockLine, RiEyeLine, RiEyeOffLine} from "react-icons/ri"
 import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 
 const Contrasenna = () => {
 
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [user, setUser] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
     documento:'V',
   })
 
-  const onChangeHandler = (event) => {
+  const {register, handleSubmit,
+    formState: {errors} 
+  } = useForm();
 
-    console.log(event)
-    if (event.target.name === 'languages') {
-
-      let copy = { ...formData }
-
-      if (event.target.checked) {
-        copy.languages.push(event.target.value)
-      } else {
-        copy.languages = copy.languages.filter(el => el !== event.target.value)
-      }
-
-      setFormData(copy)
-
-    } else {
-      setFormData(() => ({
-        ...formData,
-        [event.target.name]: event.target.value
-      }))
-    }
-  }
-
-  const onSubmitHandler = (event) => {
-    event.preventDefault()
-    console.log(formData)
-  }
+  const onSubmit = handleSubmit ((data) => {
+    console.log(data);
+  });
 
   const handleShowPassword = () => {
     setShowPassword (!showPassword);
@@ -53,27 +29,6 @@ const Contrasenna = () => {
     setShowConfirmPassword (!showConfirmPassword);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    if([user, password].includes("")){
-      toast.error("Todos los campos son obligatorios", {
-        theme: "dark",
-        position: "top-center"
-      });
-      return;
-    }
-
-    if(password.length < 8){
-      toast.error("La contraseña debe contener al menos 8 caracteres", {
-        theme: "dark",
-        position: "top-center"
-      });
-      return;
-    }
-
-  }
-
   return (
     <div className="bg-[url('/src/assets/registro.jpg')] bg-no-repeat bg-cover bg-center bg-fixed flex items-center justify-center min-h-screen">
         <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
@@ -82,19 +37,17 @@ const Contrasenna = () => {
               <span className="mb-3 text-3xl font-bold font-Montserrat text-primary">Bienvenido a</span>
               <span className="mb-3 text-4xl font-bold font-Montserrat text-primary">Gestión de Contraseña</span>
               <span className="font-light text-gray-600 mb-8 text-xl">Por favor ingresa tus datos</span>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={onSubmit}>
               
             <div className="grid grid-cols-2 gap-3">
 
             <div className='col-end-1'>
-              <form onSubmit={onSubmitHandler}>
                 <div className="form-group py-4">
-                  <select className="form-select border rounded-lg py-3 px-3 text-gray-400" name="documento" onChange={onChangeHandler} value={formData.documento}>
+                  <select className="form-select border rounded-lg py-3 px-3 text-gray-400" name="documento">
                     <option value="v">V</option>
                     <option value="e">E</option>
                   </select>
                 </div>
-              </form>
             </div>
 
             <div className="py-4 col-end-2">
@@ -102,19 +55,53 @@ const Contrasenna = () => {
                 type="text"
                 className="py-3 w-full border border-gray-200 outline-none px-4 rounded-lg focus:border-primary"
                 placeholder="Cédula"
-              />
+                name="cedula"
+                {...register("cedula", {
+                  required: {
+                    value: true,
+                    message: "Por favor ingrese cédula"
+                  },
+                  pattern: {
+                    value: /^[0-9]+[0+9]*$/,
+                    message: "Cédula no válida"
+                  },
+                  maxLength: {
+                    value: 8,
+                    message: "Cédula incorrecta"
+                  },
+                  minLength: {
+                    value: 8,
+                    message: "Cédula incorrecta"
+                  },
+                })} 
+                /> 
+                {
+                  errors.cedula && <span className="text-sm text-red-600">{errors.cedula.message}</span>
+                }                                   
             </div>
             </div>
 
               <div className="relative py-4">
-                <RiUser3Line className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500"/>
+                <RiMailLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500"/>
                 <input
                   type="text"
                   className="py-3 w-full border border-gray-200 outline-none px-8 rounded-lg focus:border-primary"
-                  placeholder="Usuario"
-                  value={user}
-                  onChange={(e) => setUser(e.target.value)}
+                  placeholder="Correo electrónico"
+                  name="mail"
+                  {...register("mail", {
+                    required: {
+                      value: true,
+                      message: "Por favor ingrese correo electrónico"
+                    },
+                    pattern: {
+                      value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                      message: "Correo no válido",
+                    },
+                  })}
                 />
+                {
+                  errors.mail && <span className="text-sm text-red-600">{errors.mail.message}</span>
+                }
               </div>
 
               <div className="relative py-4">
@@ -123,9 +110,25 @@ const Contrasenna = () => {
                   type={showPassword ? "text" : "password"}
                   className="py-3 w-full border border-gray-200 outline-none px-8 rounded-lg focus:border-primary"
                   placeholder="Nueva contraseña"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
+                  name="password"
+                  {...register("password", {
+                    required: {
+                      value: true,
+                      message: "Por favor ingrese contraseña"
+                    },
+                    minLength: {
+                      value: 8,
+                      message: "Contraseña incorrecta"
+                    },
+                    maxLength: {
+                      value: 8,
+                      message: "Contraseña incorrecta"
+                    },
+                  })}                  
                 /> 
+              {
+                errors.password && <span className="text-sm text-red-600">{errors.password.message}</span>
+              }                
                 {showPassword ? (
                   <RiEyeOffLine onClick={handleShowPassword} className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer"/>
 
@@ -139,9 +142,25 @@ const Contrasenna = () => {
                   type={showConfirmPassword ? "text" : "password"}
                   className="py-3 w-full border border-gray-200 outline-none px-8 rounded-lg focus:border-primary"
                   placeholder="Repetir contraseña"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                /> 
+                  name="confirmPassword"
+                  {...register("confirmPassword", {
+                    required: {
+                      value: true,
+                      message: "Por favor repita la contraseña"
+                    },
+                    minLength: {
+                      value: 8,
+                      message: "Contraseña incorrecta"
+                    },
+                    maxLength: {
+                      value: 8,
+                      message: "Contraseña incorrecta"
+                    },
+                  })}                  
+                />
+              {
+                errors.confirmPassword && <span className="text-sm text-red-600">{errors.confirmPassword.message}</span>
+              }                 
                 {showPassword ? (
                   <RiEyeOffLine onClick={handleShowConfirmPassword} className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer"/>
 
@@ -160,11 +179,11 @@ const Contrasenna = () => {
             <div className="flex justify-between w-full py-2">
           </div>
           <div className='grid grid-cols-2 gap-5'>
+            <button type="submit" className="w-full text-white py-3 px-6 rounded-lg mb-6 bg-secondary border-secondary">
+              Confirmar
+            </button>
             <button className="w-full text-white py-3 px-6 rounded-lg mb-6 bg-secondary border-secondary" onClick={() => navigate("/olvido")}>
               Cancelar
-            </button>
-            <button className="w-full text-white py-3 px-6 rounded-lg mb-6 bg-secondary border-secondary">
-              Confirmar
             </button>
           </div>
           </form>
