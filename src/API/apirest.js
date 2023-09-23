eea0b74bdb8515e634814e7d6f79e325795a1494
Kleiloz <jemalorued@gmail.com>
2023-09-22 22:50:06 -0400

    Login y Axios

---
+++ src/API/apirest.js
@@ -0,0 +1,2 @@
+export const Apiurl = "http://localhost:3000/";
+export const ApiLogin = "v1/public/client/user/login";
--- src/pages/IniciarSesion.jsx
+++ src/pages/IniciarSesion.jsx
@@ -1,26 +1,49 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import login from "../assets/login.jpg";
-import {RiMailLine, RiLockLine, RiEyeLine, RiEyeOffLine} from "react-icons/ri"
+import { RiMailLine, RiLockLine, RiEyeLine, RiEyeOffLine } from "react-icons/ri"
 import { useNavigate } from 'react-router-dom';
 import { useForm } from "react-hook-form";
+import { Apiurl,ApiLogin } from '../API/apirest';
 
+import axios from 'axios';
 
 const IniciarSesion = () => {
-
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
+  const [showMessage, setShowMessage] = useState(false);
+  const [showMessage1, setShowMessage1] = useState(false);
+  const [showMessage2, setShowMessage2] = useState(false);
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const {register, handleSubmit,
-    formState: {errors} 
-  } = useForm();
-
-  const onSubmit = handleSubmit ((data) => {
-    console.log(data);
+  const onSubmit = handleSubmit((data) => {
+    if (!Object.keys(errors).length) {
+      setShowMessage(true);
+      let url = Apiurl + ApiLogin;
+      axios.post(url, data)
+        .then((response) => {
+          if (response.statusText === "OK") {
+            localStorage.setItem("JWT", response.data.data.jwt);
+            navigate("/homebanca")
+          } else if (response.data === null) {
+            setShowMessage(false);
+            setShowMessage1(true);
+            setTimeout(() => {
+              window.location.reload();
+            }, 3000);
+          }
+        }).catch(error => {
+          console.log(error);
+          setShowMessage2(true);
+          setTimeout(() => {
+            window.location.reload();
+          }, 5000);
+        })
+    }
   });
 
   const handleShowPassword = () => {
-    setShowPassword (!showPassword);
+    setShowPassword(!showPassword);
   };
 
   return (
@@ -29,18 +52,18 @@ const IniciarSesion = () => {
       <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
         {/* left side */}
         <div className="flex flex-col justify-center p-8 md:p-14">
-            <span className="mb-3 text-3xl font-bold font-Montserrat text-primary">Bienvenido a</span>
-            <span className="mb-3 text-4xl font-bold font-Montserrat text-primary">Banca en Línea</span>
-            <span className="  font-light text-gray-600 mb-8 text-xl">Por favor ingresa tus datos</span>
+          <span className="mb-3 text-3xl font-bold font-Montserrat text-primary">Bienvenido a</span>
+          <span className="mb-3 text-4xl font-bold font-Montserrat text-primary">Banca en Línea</span>
+          <span className="  font-light text-gray-600 mb-8 text-xl">Por favor ingresa tus datos</span>
           <form onSubmit={onSubmit}>
             <div className="relative py-4">
-              <RiMailLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500"/>
+              <RiMailLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500" />
               <input
                 type="text"
                 className="py-3 w-full border border-gray-200 outline-none px-8 rounded-lg focus:border-primary flex"
                 placeholder="Correo electrónico"
-                name="mail"
-                {...register("mail", {
+                name="email"
+                {...register("email", {
                   required: {
                     value: true,
                     message: "Por favor ingrese correo electrónico"
@@ -52,11 +75,11 @@ const IniciarSesion = () => {
                 })}
               />
               {
-                errors.mail && <span className="text-sm text-red-600">{errors.mail.message}</span>
+                errors.email && <span className="text-sm text-red-600">{errors.email.message}</span>
               }
             </div>
             <div className="relative py-4">
-              <RiLockLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500"/>
+              <RiLockLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500" />
               <input
                 type={showPassword ? "text" : "password"}
                 className="py-3 w-full border border-gray-200 outline-none px-8 rounded-lg focus:border-primary flex"
@@ -65,61 +88,78 @@ const IniciarSesion = () => {
                 {...register("password", {
                   required: {
                     value: true,
-                    message: "Por favor ingrese contraseña"
+                    message: 'Por favor ingrese contraseña',
                   },
-                  minLength: {
-                    value: 8,
-                    message: "Contraseña incorrecta"
-                  },
-                  maxLength: {
-                    value: 8,
-                    message: "Contraseña incorrecta"
+                  pattern: {
+                    value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/,
+                    message:
+                      'La contraseña es incorrecta.',
                   },
                 })}
-              /> 
+              />
               {
                 errors.password && <span className="text-sm text-red-600">{errors.password.message}</span>
               }
               {showPassword ? (
-                <RiEyeOffLine onClick={handleShowPassword} className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer"/>
+                <RiEyeOffLine onClick={handleShowPassword} className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer" />
 
-              ) : ( <RiEyeLine onClick={handleShowPassword} className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer"/>
-            )}
+              ) : (<RiEyeLine onClick={handleShowPassword} className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer" />
+              )}
             </div>
-          <div className="flex justify-between w-full py-4">
-            <span className=" text-primary text-md"><Link to={"/olvido"}>Olvidé mi correo o contraseña</Link></span>
+            <div className="flex justify-between w-full py-4">
+              <span className=" text-primary text-md"><Link to={"/olvido"}>Olvidé mi correo o contraseña</Link></span>
             </div>
             <button type="submit" className="w-full text-white py-3 px-6 rounded-lg mb-6 bg-secondary border-secondary">
-               Iniciar sesión
+              Iniciar sesión
             </button>
             <p className="text-sm" onClick={() => navigate("/homebanca")}>Ir a la banca</p>
-        </form>
-        <div className="text-center text-gray-400">
-          ¿No tienes cuenta? 
-          <span className="font-bold text-primary"> 
-            <Link to={"/registro"}> Regístrate</Link>
-          </span>
+          </form>
+          <div className="text-center text-gray-400">
+            ¿No tienes cuenta?
+            <span className="font-bold text-primary">
+              <Link to={"/registro"}> Regístrate</Link>
+            </span>
+          </div>
+
+          <div className='mt-10 text-center text-base'>
+            <span className="font-light text-primary">
+              <Link to={"/home"}> Volver a home</Link>
+            </span>
+          </div>
+
         </div>
 
-        <div className='mt-10 text-center text-base'>
-          <span className="font-light text-primary"> 
-            <Link to={"/home"}> Volver a home</Link>
-          </span>
-        </div>
-      
-      </div>
         {/* right side */}
         <div className="relative">
-        <img
-          src={login}
-          alt="img"
-          className="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"
-        />
+          <img
+            src={login}
+            alt="img"
+            className="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"
+          />
         </div>
       </div>
-
+      {showMessage && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 flex">
+          <div className="bg-white p-6 rounded-lg">
+            <p className="text-center text-xl font-semibold">Verificando Información...</p>
+          </div>
+        </div>
+      )}
+      {showMessage1 && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 flex">
+          <div className="bg-white p-6 rounded-lg">
+            <p className="text-center text-xl font-semibold">El correo o la contraseña son incorrectos...</p>
+          </div>
+        </div>
+      )}
+      {showMessage2 && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 flex">
+          <div className="bg-white p-6 rounded-lg">
+            <p className="text-center text-xl font-semibold">Error con el servidor</p>
+          </div>
+        </div>
+      )}
     </div>
-
   );
 };
 
